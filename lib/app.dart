59178905df18a2fdd49a1fe1f61204709aeb442c
diazsasak <diazfebrian2@gmail.com>
2020-10-31 16:03:04 +0800

    disable device preview

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gank_flutter/blocs/authentication/authentication_bloc.dart';
@@ -40,32 +39,31 @@ class _AppState extends State<App> {
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          locale: DevicePreview.of(context).locale,
-          builder: DevicePreview.appBuilder,
           theme: theme,
           navigatorKey: _navigatorKey,
-          home: BlocListener<AuthenticationBloc, AuthenticationState>(
-            listener: (context, state) {
-              print('yes');
-              switch (state.status) {
-                case AuthenticationStatus.authenticated:
-                  _navigator.pushAndRemoveUntil<void>(
-                    HomePage.route(),
-                    (route) => false,
-                  );
-                  break;
-                case AuthenticationStatus.unauthenticated:
-                  _navigator.pushAndRemoveUntil<void>(
-                    LoginPage.route(),
-                    (route) => false,
-                  );
-                  break;
-                default:
-                  break;
-              }
-            },
-            child: SplashPage(),
-          ),
+          builder: (context, child) {
+            return BlocListener<AuthenticationBloc, AuthenticationState>(
+              listener: (context, state) {
+                switch (state.status) {
+                  case AuthenticationStatus.authenticated:
+                    _navigator.pushAndRemoveUntil<void>(
+                      HomePage.route(),
+                      (route) => false,
+                    );
+                    break;
+                  case AuthenticationStatus.unauthenticated:
+                    _navigator.pushAndRemoveUntil<void>(
+                      LoginPage.route(),
+                      (route) => false,
+                    );
+                    break;
+                  default:
+                    break;
+                }
+              },
+              child: child,
+            );
+          },
           onGenerateRoute: (_) => SplashPage.route(),
         ),
       ),
